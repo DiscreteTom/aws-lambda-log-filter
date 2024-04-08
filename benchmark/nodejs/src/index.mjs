51d4ea523566e881c064519118c0d2593a16a8cb
DiscreteTom <discrete_tom@outlook.com>
2024-04-08 07:17:33 +0000

    bench: add ADD_CPU_TASK, disabled by default

--- benchmark/nodejs/src/index.mjs
+++ benchmark/nodejs/src/index.mjs
@@ -1,15 +1,49 @@
 import { faker } from "@faker-js/faker";
 
 const LOG_COUNT = Number(process.env.LOG_COUNT);
+const ADD_CPU_TASK = process.env.ADD_CPU_TASK == "true";
 
 export const handler = async () => {
+  let cpuTime = 0;
   for (let i = 0; i < LOG_COUNT; ++i) {
+    // run some cpu bound task to simulate real life lambda tasks
+    if (ADD_CPU_TASK) {
+      const start = performance.now();
+      calculatePrimes(1000);
+      cpuTime += performance.now() - start;
+    }
+
     // use process.stdout to avoid prepending other info
     process.stdout.write(faker.git.commitEntry() + "\n");
   }
+
+  if (ADD_CPU_TASK) {
+    process.stdout.write(`total cpu time: ${cpuTime}ms\n`);
+  }
+
+  process.stdout.write(`done\n`);
 
   return {
     statusCode: 200,
     body: "hello",
   };
 };
+
+function calculatePrimes(n) {
+  let primes = [];
+  for (let i = 2; i <= n; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+  return primes;
+}
+
+function isPrime(num) {
+  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
